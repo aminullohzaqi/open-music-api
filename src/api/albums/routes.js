396a4d4d7a4ec2b00af69ee9990befec1591250f
Aminulloh Zaqi <aminulloh.zaqi@gmail.com>
2022-04-02 20:39:46 +0700

    add album like feature

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -44,6 +44,19 @@ const routes = (handler) => [
                 path: path.resolve(__dirname, 'file')
             }
         }
+    },
+    {
+        method: 'POST',
+        path: '/albums/{id}/likes',
+        handler: handler.postAlbumLikeHandler,
+        options: {
+            auth: 'openmusicapp_jwt'
+        }
+    },
+    {
+        method: 'GET',
+        path: '/albums/{id}/likes',
+        handler: handler.getAlbumLikesHandler
     }
 ]
 
